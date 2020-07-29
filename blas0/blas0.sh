4588297241a5499fb0892da9cc3a28ef4a28a444
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-29 14:49:15 +0530

    BASH commands to compile the source code.

---
+++ blas0/blas0.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+mkdir temp
+cd temp
+rm *
+~/binc/f77split ../blas0.f
+#
+for FILE in `ls -1 *.f`;
+do
+  gfortran -c $FILE
+  if [ $? -ne 0 ]; then
+    echo "Errors compiling " $FILE
+    exit
+  fi
+done
+rm *.f
+#
+ar r ~/libf77/libblas.a *.o
+rm *.o
+cd ..
+rmdir temp
+#
+echo "Library installed as ~/libf77/libblas.a."
